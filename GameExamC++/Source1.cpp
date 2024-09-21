fae3fe6cc89778b6b6b0870887967b4a0e01565d
Vladisley666 <kluckovlad445@gmail.com>
2024-09-21 19:19:36 +0300

    ))))

--- GameExamC++/Source1.cpp
+++ GameExamC++/Source1.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
+
+#include<iostream>
+#include<iomanip>
+#include<Windows.h>
+
+
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello World!";
+	cout << "Hello World!" << endl;
+
+
+
+
+
+
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << ")" << endl;
+	}
+
+
+
+
+
+
     return 0;
 }
+
+
+
