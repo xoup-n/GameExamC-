9fe96ecb8a1bf1e125fb31a237c2760d6c34a150
культ шаловливых <110924875+xoup-n@users.noreply.github.com>
2024-09-22 14:39:27 +0300

    1

---
+++ Funcs.h
@@ -0,0 +1,32 @@
+
+#pragma once
+#include <string>
+using namespace std;
+
+char* SplitWord(string word)
+{
+
+    int len = word.length();
+    char* letters = new char[len];
+    for (int i = 0; i < len; i++)
+    {
+        letters[i] = word[i];
+    }
+    return letters;
+}
+
+//void checking(char masiw, string word, char letter)
+//{
+//    int number = word.length();
+//    for (int i = 0; i < number; i++)
+//    {
+//        if (letter == masiw[i])
+//        {
+//            cout << letter << endl;
+//        }
+//
+//    }
+//
+//}
+//h  e  l  l  o
+//[[h],[e],[l],[l],[o]]
